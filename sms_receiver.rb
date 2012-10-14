51c923db55c5fa56bf23f3ef2dd4880569381923
Artem Titoulenko <artem.titoulenko@gmail.com>
2012-10-14 11:23:25 -0400

    funcitonal part

--- sms_receiver.rb
+++ sms_receiver.rb
@@ -15,16 +15,40 @@ Mongoid.load!("mongoid.yml")
 # create a twilio client
 client = Twilio::REST::Client.new(@account_sid, @auth_token)
 
+# table for day of the week into a number
+days = { sunday: 0,
+         monday: 1,
+         tuesday: 2,
+         wednesday: 3,
+         thursday: 4,
+         friday: 5,
+         saturday: 6 }
+
+# protocol:
+#   task on day of the week at time, day of the week at time, etc.
+
+# day of the week must be monday, tuesday, wednesday, thursday, friday
+# time must be an hour and must have either an am/pm or be in 24 hour format
+def split_into_task_and_time(text)
+  task_and_time = text.split(" on ")
+  task_and_time[1] = task_and_time[1].scan(/(monday|tuesday|wednesday|thursday|friday) at (\d{1,2})(?:am|pm|)/)
+  return task_and_time
+end
+
 # callback for SMS receive
 post '/' do
-  to = params["From"].to_s
+  t_and_t = split_into_task_and_time(params["Body"])
 
-  message = {:from => '+17328124972', 
-             :to => to,  
-             :body => 'Your text.'}
+  t_and_t[1].each do |pair|
+    r = Reminder.new(task: t_and_t[0],
+                     day: days[pair[0].to_sym],
+                     hour: pair[1].to_i,
+                     phone: params["From"])
 
-  @account = client.account
-  @message = @account.sms.messages.create(message)
+    puts r
+
+    r.save!
+  end
 end
 
 # Callback for when people visit the site from their browser
